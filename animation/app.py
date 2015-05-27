f75c76a965fe1b311475bd5f5c606630e9c468a7
Yusuf kelo <yusufkel@gmail.com>
2015-05-27 20:18:58 +0100

    update

--- animation/app.py
+++ animation/app.py
@@ -1,10 +1,23 @@
 from flask import Flask
 from flask import render_template
+from flask import redirect, url_for,request,json
 app=Flask(__name__)
 
 
 @app.route("/")
 def index():
 	return render_template("index.html")
+	
+@app.route("/home")
+def home():
+	return render_template("home.html")	
+	
+@app.route('/save',methods=['POST'])
+def save():
+	#return "saved"
+	return redirect(url_for("home"))
+	
+
+	
 
 app.run(debug=True,port=8000,host='0.0.0.0')
