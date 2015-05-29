e97ea88590a12b7eee49d10672c32e7e63cc7a72
ykel <yusufkelo@outlook.com>
2015-05-28 23:28:14 -0700

    cookie

--- animation/app.py
+++ animation/app.py
@@ -1,12 +1,17 @@
 from flask import Flask
 from flask import render_template
-from flask import redirect, url_for,request,json
+from flask import (redirect, url_for,
+                   request,json,make_response)
+
+
 app=Flask(__name__)
+
 
 
 @app.route("/")
 def index():
-	return render_template("index.html")
+	data=get_cookie()
+	return render_template("index.html",saves=data)
 	
 @app.route("/home")
 def home():
@@ -15,8 +20,20 @@ def home():
 @app.route('/save',methods=['POST'])
 def save():
 	#return "saved"
-	return redirect(url_for("home"))
+	response=make_response(redirect(url_for("index")))
+	data=get_cookie()
+	data.update(dict(request.form.items()))
+	response.set_cookie('character',json.dumps(data))
+	return response
 	
+
+		
+def get_cookie():
+	try:
+		data=json.loads(request.cookies.get('character'))
+	except TypeError:
+		data={}
+	return data
 
 	
 
